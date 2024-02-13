70986a447db42923818d48b9fb86fbd3c753fc9c
EvgenSakun <evgen.sakun20@gmail.com>
2024-02-13 19:39:09 +0300

    start

---
+++ HM21/HM21/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HM21
+//
+//  Created by Evgeny Sakun on 13.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+    
+    
+    
+}
+
